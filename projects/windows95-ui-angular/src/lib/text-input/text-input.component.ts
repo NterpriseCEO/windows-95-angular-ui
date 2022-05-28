b4f66fb92d4b45daa3c52ee92d90c50242fb15de
NterpriseCEO <nuzumalex@gmail.com>
2022-05-28 01:48:35 +0100

    Text input component

---
+++ projects/windows95-ui-angular/src/lib/text-input/text-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+	selector: "text-input",
+	template: `<div class="text-input-wrapper" [class.disabled] = "disabled">
+					<input
+						type = "text"
+						[disabled] = "disabled"
+						[placeholder] = "placeholder"
+						[value] = "value"
+						(keyup) = "changeValue($event.target)"
+						(keydown.backspace) = "changeValue($event.target)"
+					>
+				</div>`,
+	styleUrls: ["text-input.component.scss"]
+})
+export class TextInput {
+
+	@Input() disabled: boolean = false;
+	@Input() placeholder: string = "";
+	@Input() value: string = "";
+	@Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+	constructor() {}
+
+	changeValue(event: any) {
+		console.log(event.value);
+		
+		this.valueChange.emit(event.value);
+	}
+}
---
+++ projects/windows95-ui-angular/src/lib/text-input/text-input.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { TextInput } from './text-input.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    TextInput
+  ],
+  imports: [
+    FormsModule
+  ],
+  exports: [
+    TextInput
+  ]
+})
+export class TextInputModule { }
